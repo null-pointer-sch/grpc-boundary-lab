9dfdf2b53dd7ce919d58a83f92f60bffc5a3d480
Andreas Schubert <andy.schubert@outlook.com>
2026-02-20 20:16:35 +0100

    refactor: reorganize project into src and deploy directories

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,10 @@
 rootProject.name = "grpc-boundary-lab"
 
 include("proto")
+project(":proto").projectDir = file("src/proto")
 include("backend")
+project(":backend").projectDir = file("src/backend")
 include("gateway")
+project(":gateway").projectDir = file("src/gateway")
 include("loadgen")
+project(":loadgen").projectDir = file("src/loadgen")
